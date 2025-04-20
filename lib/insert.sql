9a9e7097fed1eae0fbe238a3bb7a4b4683af268e
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-04-20 19:48:15 +0300

    commit sql-organizing-bears-lab

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 8, 'M', 'brown', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 10, 'M', 'brown', 'friendly', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 3, 'F', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 6, 'M', 'brown', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 4, 'F', 'black', 'friendly', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 8, 'M', 'brown', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 4, 'M', 'brown', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 8, 'F', 'black', 'calm', 1);
+
